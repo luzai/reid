fix backprop chain rule for triplet loss

reid/loss/triplet.py
@@ -363,7 +363,6 @@ class TripletLoss(nn.Module):
                 dist_ap.append(((xa - xp) ** 2).sum().clamp(min=1e-12, max=1e12).sqrt())
                 dist_an.append(((xa - xn) ** 2).sum().clamp(min=1e-12, max=1e12).sqrt())
 
-
             elif self.mode == 'adap':
                 some_pos = dist[i][mask[i]]
                 some_neg = dist[i][mask[i] == 0]
@@ -397,9 +396,15 @@ class TripletLoss(nn.Module):
                 negp = dist[i][mask[i] == 0]
                 dist_an.append(negp[np.random.randint(0, negp.size(0))])
 
-        dist_ap = _concat(dist_ap) * self.margin2
-        dist_an = _concat(dist_an) / self.margin3
+        dist_ap = _concat(dist_ap)
+        dist_an = _concat(dist_an)
         y = torch.ones(dist_an.size(), requires_grad=False).cuda()
+
+        assert self.margin == 'soft', 'only soft now'
+        pre_loss = F.softplus(dist_ap - dist_an).mean().detach()
+        coff = torch.exp(pre_loss) / (1 + torch.exp(pre_loss))
+        dist_ap *= (coff * self.margin2)
+        dist_an /= (coff * self.margin3)
         if self.margin != 'soft':
             loss = self.ranking_loss(dist_an, dist_ap, y)
         else:

reid/utils/data/preprocessor.py
@@ -4,6 +4,30 @@ from lz import *
 # from lomo.lomo_map import extract_feature
 from torch.utils.data import Dataset
 import copy
+from itertools import chain
+
+
+class DirPreprocessor(object):
+    def __init__(self, dir, transorm):
+        self.dir = dir
+        self.imgs = glob.glob(dir + '/*.jpg') + \
+                    glob.glob(dir + '/*.png') + \
+                    glob.glob(dir + '/*.JPG')
+
+        self.transform = transorm
+
+    def __len__(self):
+        return len(self.imgs)
+
+    def __getitem__(self, indices):
+        if isinstance(indices, (tuple, list)):
+            return [self._get_single_item(index) for index in indices]
+        else:
+            return self._get_single_item(indices)
+
+    def _get_single_item(self, index):
+        imgp = self.imgs[index]
+        return self.transform(read_image(imgp)), imgp
 
 
 class Preprocessor(object):
@@ -72,7 +96,7 @@ class Preprocessor(object):
         return res_return
 
 
-import lmdb,six
+import lmdb, six
 
 root = '/home/xinglu/.torch/data/mars/img_lmdb'
 osp.exists(root)

tools/show_tsne.py
@@ -2,7 +2,7 @@ from lz import *
 import cv2
 from sklearn.manifold import TSNE
 
-feas_dict = msgpack_load(work_path + '/reid.person/fea.mp', allow_np=True)
+feas_dict = msgpack_load(work_path + 'tmp.mp', allow_np=True)
 
 limit_size = 5000
 feask = list(feas_dict.keys())[:limit_size]
@@ -20,19 +20,20 @@ feas_normalized = feas / feas_norm
 embed2 = TSNE(n_components=2).fit_transform(
     feas_normalized
 )
+
 height = 128
 width = 64
-embed2 -= embed2.min(axis=0)
+embed2 = embed2 - embed2.min(axis=0)
 # np.median(np.abs(np.diff(embed2, axis=0)), axis=0)
-embed2 *= width
+space = 48
+embed2 *= space
 embed2 = embed2.astype(int)
 extend = np.array([height, width, ])
-# embed2 += extend
 
 shape = tuple(
     (embed2.max(axis=0).astype(int) + extend).tolist()
 ) + (3,)
-print(shape)
+print('res shape', shape)
 res = np.ones(
     shape
 ).astype(np.uint8) * 255
@@ -40,14 +41,23 @@ res = np.ones(
 for ind in range(feas.shape[0]):
     img_name = feask[ind]
     img = cv2.imread(img_name)
-    img = cv2.resize(img, (width, height))
+    img2 = cvb.resize_keep_ar(img, height, width)
+    if not (img2.shape[0] <= height and img2.shape[1] <= width):
+        img2 = cvb.resize_keep_ar(img, width, width)
+    assert (img2.shape[0] <= height and img2.shape[1] <= width)
+    # if img2.shape[0] < height:
+    #     img2 = np.concatenate((img2, np.ones((height - img2.shape[0], img2.shape[1], 3)) * 255), axis=0)
+    # if img2.shape[1] < width:
+    #     img2 = np.concatenate((img2, np.ones((img2.shape[0], width - img2.shape[1], 3)) * 255), axis=1)
+    # assert img2.shape[0] == height and img2.shape[1] == width
+    img = img2
     embed = embed2[ind].astype(int)
     row, col = embed
     h, w, _ = img.shape
     res[row:row + h, col:col + w] = img
-    if ind > 10:
-        break
+    # if ind > 10:
+    #     break
 # cv2.namedWindow('tmp', cv2.WINDOW_NORMAL)
 # cv2.imshow('tmp', res)
 # cv2.waitKey(0)
-cv2.imwrite(work_path + '/tmp3.jpg', res)
+cv2.imwrite(work_path + '/tmp.jpg', res)