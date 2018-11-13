ad4d34e9b0179294fa1b977d7f76d0a8393527b5
Xinglu Wang <907682447@qq.com>
2018-11-13 20:35:25 +0800

    cuhk03 need resize to obtain full image information! (todo can I use padding and keep aspect ratio?)

--- reid/utils/data/transforms.py
+++ reid/utils/data/transforms.py
@@ -20,25 +20,58 @@ class RectScale(object):
         w, h = img.size
         if h == self.height and w == self.width:
             return img
-        # way 1:
-        ratio_w = self.width / w
-        ratio_h = self.height / h
-        ratio = max(ratio_h, ratio_w)
-        target_size = (int(w * ratio), int(h * ratio))
-        img = img.resize(target_size, self.interpolation)  # resize if width height
-        img = CenterCrop((self.height, self.width))(img)
-        # pytorch transofrm Resize is height width
-        # img is width height
-        return img
+        ## way 1:
+        # ratio_w = self.width / w
+        # ratio_h = self.height / h
+        # ratio = max(ratio_h, ratio_w)
+        # target_size = (int(w * ratio), int(h * ratio))
+        # img = img.resize(target_size, self.interpolation)  # resize if width height
+        # img = CenterCrop((self.height, self.width))(img)
+        # # pytorch transofrm Resize is height width
+        # # img is width height
 
-        # way 2:
-        # direct resize
+        ## todo way 2: direct resize
+        img = img.resize((self.width, self.height), self.interpolation)
+        return img
 
 
 # todo more aug, try random erasing
 
+class RandomCropFlip3(object):
+    """
+    With a probability, first increase image size to (1 + 1/8), and then perform random crop.
+    Args:
+    - height (int): target image height.
+    - width (int): target image width.
+    - p (float): probability of performing this transformation. Default: 0.5.
+    """
+
+    def __init__(self, height, width, interpolation=Image.BILINEAR, p=0.5, **kwargs):
+        self.height = height
+        self.width = width
+        self.p = p
+        self.interpolation = interpolation
+
+    def __call__(self, img):
+        """
+        Args:
+        - img (PIL Image): Image to be cropped.
+        """
+        if random.uniform(0, 1) > self.p:
+            return img.resize((self.width, self.height), self.interpolation)
+
+        new_width, new_height = int(round(self.width * 1.125)), int(round(self.height * 1.125))
+        resized_img = img.resize((new_width, new_height), self.interpolation)
+        x_maxrange = new_width - self.width
+        y_maxrange = new_height - self.height
+        x1 = int(round(random.uniform(0, x_maxrange)))
+        y1 = int(round(random.uniform(0, y_maxrange)))
+        croped_img = resized_img.crop((x1, y1, x1 + self.width, y1 + self.height))
+        return croped_img
+
+
 class RandomCropFlip(object):
-    def __init__(self, height, width, interpolation=Image.BILINEAR, area=(.75, 1),
+    def __init__(self, height, width, interpolation=Image.BILINEAR, area=(.85, 1),
                  aspect=(1.5, 3)):  # 0.64, 1 # .85, 1  # 2, 3  # todo
         self.height = height
         self.width = width
@@ -71,7 +104,6 @@ class RandomCropFlip(object):
 
                 img = img.crop((x1, y1, x1 + w, y1 + h))
                 assert (img.size == (w, h))
-                # return img.resize((self.width, self.height), self.interpolation)
                 return self.scale(img)
         # Fallback
         return self.scale(img)
